f39b05c218710d6a7b28656ce10b7b0f4a170efb
Transyl <trans74yl@gmail.com>
2022-08-18 21:59:46 +0200

    1

---
+++ Assets/Scripts/CartFunc.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class CartFunc : MonoBehaviour
+{
+    //3x^3+2x^2-3x+1
+    //sin(1)+3*x^3 + 2*x^2 -3x +1
+    public void DrawFunc(string val0)
+    {
+
+    }
+}
